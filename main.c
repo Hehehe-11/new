6eed5791c29445d6a93ee30be41e4c7ae20a42fc
Hehehe-11 <dyumna.negi1@gmail.com>
2024-10-16 21:55:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  palindrome
+//
+//  Created by Dyumna on 20/07/24.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("enter a number");
+    int n;
+    scanf("%d", &n);
+    int rem, sum=0, rev;
+    rev=n;
+    while(n>0)
+    {
+        rem=n%10;
+        sum=sum*10+ rem;
+        n=n/10;
+    }
+    if(sum==rev)
+        printf("palindrome");
+    else
+        printf("not pal");
+    return 0;
+}
